ff8371b780907731ce7d45a386a42c3a2d33edf9
Ramonprog <programador.ramon@gmail.com>
2024-06-01 20:25:23 -0300

    feat: setando cach para cidades por estado

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { CityEntity } from './entities/city.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { Cache } from 'cache-manager'
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
   async getAllCitiesByStateID(stateId: number): Promise<CityEntity[]> {
-    return await this.cityRepository.find({
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      String(stateId),
+    )
+
+    if (citiesCache) {
+      return citiesCache
+    }
+
+    const cities = await this.cityRepository.find({
       where: {
         stateId,
       },
     })
+
+    await this.cacheManager.set(String(stateId), cities)
+
+    return cities
   }
 }
